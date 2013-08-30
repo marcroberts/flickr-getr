9514b258d2d7318ced66d18c3dcfc662434a1eaf
Marc Roberts <marc@neutroncreations.com>
2013-08-30 16:35:33 +0100

    move api key to argument, update to 1.0.0

--- flickr-get.rb
+++ flickr-get.rb
@@ -2,7 +2,7 @@
 
 # flickr-getr
 #
-# version: 0.1
+# version: 1.0.0
 
 require 'rubygems'
 require 'httparty'
@@ -13,13 +13,16 @@ class Flickr
   include HTTParty
   base_uri 'api.flickr.com'
   format :json
-  
-  
+
+  def initialize key
+    @key = key
+  end
+
   def photos set_id
-    
+
     query = {
       :method => 'flickr.photosets.getPhotos',
-      :api_key => 'cedccf118bf9c2395a8f40005ad59c57',
+      :api_key => @key,
       :photoset_id => set_id,
       :per_page => 500,
       :extras => 'date_taken,original_format',
@@ -27,40 +30,40 @@ class Flickr
       :format => 'json',
       :nojsoncallback => 1
     }
-    
+
     self.class.get('/services/rest/', :query => query)
   end
-  
+
 end
 
 # ----------------
 
-if ARGV.count != 2
-  print "Usage: flickr-get <path> <photoset-id>"
+if ARGV.count != 3
+  print "Usage: flickr-get <apikey> <path> <photoset-id>"
   exit
 end
 
-ROOT = ARGV[0]
-SET_ID = ARGV[1]
+ROOT = ARGV[1]
+SET_ID = ARGV[2]
 
-flickr = Flickr.new
+flickr = Flickr.new ARGV[0]
 
 photos = flickr.photos(SET_ID)
 
 photos['photoset']['photo'].each do |photo|
-  filename = File.join(ROOT, "#{photo['id']}.jpg")
+  filename = File.join(ROOT, "#{photo['id']}.#{photo['originalformat']}")
   unless File.exist?(filename)
-    
+
     url = "http://farm#{photo['farm']}.static.flickr.com/#{photo['server']}/#{photo['id']}_#{photo['originalsecret']}_o.#{photo['originalformat']}\n"
     date = Time.parse(photo['datetaken'])
-    
+
     File.open(filename, 'w') do |file|
       file.binmode
       file.write(open(url).read)
     end
-    
+
     File.utime(date, date, filename)
-    
+
   end
-  
+
 end
